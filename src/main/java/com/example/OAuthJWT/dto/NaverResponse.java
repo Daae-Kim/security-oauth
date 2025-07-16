7fa19dbc0b54da71af47fe737fc0d09a0275ad59
Daae-Kim <wlsemrdl1212@naver.com>
2025-07-16 23:42:55 +0900

    refactor : NaverResponse dto 세부 구현

--- src/main/java/com/example/OAuthJWT/dto/NaverResponse.java
+++ src/main/java/com/example/OAuthJWT/dto/NaverResponse.java
@@ -1,27 +1,33 @@
 package com.example.OAuthJWT.dto;
 
-import java.io.Serializable;
+import java.util.Map;
 
 public class NaverResponse implements OAuth2Response{
+    private final Map<String, Object> attribute;
+
+
+    public NaverResponse(Map<String, Object> attribute) {
+        this.attribute = (Map<String, Object>) attribute.get("response");
+    }
 
     @Override
     public String getProvider() {
-        return "";
+        return "naver";
     }
 
     @Override
-    public String getProviderId() {
-        return "";
+    public String getProviderId(){
+        return attribute.get("id").toString();
     }
 
     @Override
-    public String getEmail() {
-        return "";
+    public String getEmail(){
+        return attribute.get("email").toString();
     }
 
     @Override
     public String getName() {
-        return "";
+        return attribute.get("name").toString();
     }
 
 }
